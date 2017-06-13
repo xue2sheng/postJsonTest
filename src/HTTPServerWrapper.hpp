aade5806492f7d9a44309efcbe03d66a95ea338d
Andres Sevillano <andres@chameleon.ad>
2017-06-13 12:27:43 +0100

    Only header boost::asio compilation

--- src/HTTPServerWrapper.hpp
+++ src/HTTPServerWrapper.hpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <mutex>
 
+#define BOOST_ERROR_CODE_HEADER_ONLY
+#include <boost/system/error_code.hpp>
 #include <boost/asio.hpp>
-#include <boost/filesystem.hpp>
 
 class Service {
 	static const std::map<unsigned int, std::string>
